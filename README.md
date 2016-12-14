e1c63f9b6b17f6fe57e2adc5ebe4d545c1cf48af
Dylan Piercey <pierceydylan@gmail.com>
2016-12-14 11:09:11 -0800

    Switch to standard style

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-# Join-URL
+<h1 align="center">
+  <!-- Logo -->
+  Join-URL
+	<br/>
+
+  <!-- Stability -->
+  <a href="https://nodejs.org/api/documentation.html#documentation_stability_index">
+    <img src="https://img.shields.io/badge/stability-stable-brightgreen.svg?style=flat-square" alt="API stability"/>
+  </a>
+  <!-- Standard -->
+  <a href="https://github.com/feross/standard">
+    <img src="https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square" alt="Standard"/>
+  </a>
+  <!-- NPM version -->
+  <a href="https://npmjs.org/package/join-url">
+    <img src="https://img.shields.io/npm/v/join-url.svg?style=flat-square" alt="NPM version"/>
+  </a>
+  <!-- Downloads -->
+  <a href="https://npmjs.org/package/join-url">
+    <img src="https://img.shields.io/npm/dm/join-url.svg?style=flat-square" alt="Downloads"/>
+  </a>
+</h1>
+
 Like `path.join` for urls.
 
 # Installation
 
-#### Npm
 ```console
 npm install join-url
 ```
@@ -25,6 +46,6 @@ join.query("&a=1&", "b=2&", "&c=3", "d=4");
 
 ### Contributions
 
-* Use gulp to run tests.
+* Use `npm test` to run tests.
 
 Please feel free to create a PR!
--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-"use strict";
+'use strict'
 
 module.exports = {
-	pathname: joinPathName,
-	hostname: joinHostName,
-	query:    joinQuery
-};
+  pathname: joinPathName,
+  hostname: joinHostName,
+  query: joinQuery
+}
 
 /**
  * Joins paths, normalizing all slashes.
  */
 function joinPathName () {
-	// Convert arguments to an array.
-	for (var i = arguments.length, arr = new Array(i); i--;) {
-		arr[i] = arguments[i];
-	}
+  // Convert arguments to an array.
+  for (var i = arguments.length, arr = new Array(i); i--;) {
+    arr[i] = arguments[i]
+  }
 
-	return arr
-		.filter(exists)
-		.join('/')
-		.replace(/\/+/g, '/');
+  return arr
+    .filter(exists)
+    .join('/')
+    .replace(/\/+/g, '/')
 }
 
 /**
  * Joins hosts, normalizing all dots.
  */
 function joinHostName () {
-	// Convert arguments to an array.
-	for (var i = arguments.length, arr = new Array(i); i--;) {
-		arr[i] = arguments[i];
-	}
+  // Convert arguments to an array.
+  for (var i = arguments.length, arr = new Array(i); i--;) {
+    arr[i] = arguments[i]
+  }
 
-	return arr
-		.filter(exists)
-		.join('.')
-		.replace(/^\./,  '')
-		.replace(/\.+/g, '.');
+  return arr
+    .filter(exists)
+    .join('.')
+    .replace(/^\./, '')
+    .replace(/\.+/g, '.')
 }
 
 /**
  * Joins all variables in a querystring, normalizing all ampersands.
  */
 function joinQuery () {
-	// Convert arguments to an array.
-	for (var i = arguments.length, arr = new Array(i); i--;) {
-		arr[i] = arguments[i];
-	}
+  // Convert arguments to an array.
+  for (var i = arguments.length, arr = new Array(i); i--;) {
+    arr[i] = arguments[i]
+  }
 
-	return arr
-		.filter(exists)
-		.join('&')
-		.replace(/^&/,  '')
-		.replace(/&+/g, '&');
+  return arr
+    .filter(exists)
+    .join('&')
+    .replace(/^&/, '')
+    .replace(/&+/g, '&')
 }
 
 /**
  * Utility to check if a value exists.
  */
 function exists (val) {
-	return val != null
+  return val != null
 }
